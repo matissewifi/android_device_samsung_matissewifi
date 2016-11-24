b61760810a0f9f90ada543b824cfaf0f8fe87447
sub77 <sub77@ymail.com>
2016-11-24 18:48:35 +0000

    Cos a Nostra

--- cos.mk
+++ cos.mk
@@ -5,12 +5,12 @@ $(call inherit-product, device/samsung/matisse-common/matisse.mk)
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
-#$(call inherit-product, vendor/cm/config/telephony.mk)
+# Inherit some common COS stuff.
+$(call inherit-product, vendor/cos/common.mk)
+#$(call inherit-product, vendor/cos/config/telephony.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_matissewifi
+PRODUCT_NAME := cos_matissewifi
 PRODUCT_DEVICE := matissewifi
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
