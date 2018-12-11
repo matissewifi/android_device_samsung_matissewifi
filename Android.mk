e3324b8102380c3c727eb2416e386cf989086934
DJABHipHop <castellanos.kevin@rocketmail.com>
2018-12-10 23:27:21 -0500

    Fixing overlay files and adding WiFi patchFixing Overlay Files

--- Android.mk
+++ Android.mk
@@ -18,4 +18,9 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),matissewifi)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
+# Create a link for the WCNSS config file, which ends up as a writable
+# version in /data/misc/wifi
+$(shell mkdir -p $(TARGET_OUT)/etc/firmware/wlan/prima; \
+	ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
+		$(TARGET_OUT)/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
 endif
