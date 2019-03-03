7a8954c219494395164fdfd5e4837763686a7336
matteo0026 <marmotta200026@gmail.com>
2019-03-03 20:46:34 +0100

    Move full_base.mk call in matisse-common

--- full_matissewifi.mk
+++ full_matissewifi.mk
@@ -13,9 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-
 # Inherit from matissewifi device
 $(call inherit-product, device/samsung/matissewifi/device.mk)
 
