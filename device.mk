8126a848b3167cd12e32faa574055e986a9ee7f5
Kevin Castellanos <castellanos.kevin@rocketmail.com>
2019-09-24 08:53:53 -0400

    Disabling Overlays

--- device.mk
+++ device.mk
@@ -25,7 +25,7 @@ $(call inherit-product-if-exists, vendor/samsung/matissewifi/matissewifi-vendor.
 #    libshim_imx175
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+# DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # SM-T530NU
 # IR Blaster Permissions
